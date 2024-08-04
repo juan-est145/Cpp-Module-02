3caf132e55b4e1da669f2c3b2ce1a27677b8ae2f
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-08-04 18:20:49 +0200

    ex00 is done

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: juestrel <juestrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/04 17:29:59 by juestrel          #+#    #+#             */
-/*   Updated: 2024/08/04 17:32:28 by juestrel         ###   ########.fr       */
+/*   Updated: 2024/08/04 18:15:12 by juestrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,14 @@
 
 int main(void)
 {
+	Fixed a;
+	Fixed b( a );
+	Fixed c;
+
+	c = b;
+
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
 	return (0);
 }
