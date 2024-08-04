e9159344d10db1b0643fbae6658f2323f6f47524
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-08-04 18:13:24 +0200

    Implemented the Fixed class

--- ex00/Fixed.cpp
+++ ex00/Fixed.cpp
@@ -6,8 +6,45 @@
 /*   By: juestrel <juestrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/04 17:31:39 by juestrel          #+#    #+#             */
-/*   Updated: 2024/08/04 17:31:48 by juestrel         ###   ########.fr       */
+/*   Updated: 2024/08/04 18:12:30 by juestrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
+
+const int Fixed::_fractionNum = 8;
+
+Fixed::Fixed()
+{
+	std::cout << "Default constructor called" << std::endl;
+	this->_fixedNumber = 0;
+}
+
+Fixed::Fixed(const Fixed &toCopy) : _fixedNumber(toCopy._fixedNumber)
+{
+	std::cout << "Copy constructor called" << std::endl;
+}
+
+Fixed& Fixed::operator=(const Fixed &toCopy)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &toCopy)
+		this->_fixedNumber = toCopy._fixedNumber;
+	return (*this);
+}
+
+int Fixed::getRawBits(void) const 
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return (this->_fixedNumber);
+}
+
+void Fixed::setRawBits(int const raw)
+{
+	this->_fixedNumber = raw;
+}
+
+Fixed::~Fixed(void)
+{
+	std::cout << "Destructor called" << std::endl;
+}
--- ex00/Fixed.hpp
+++ ex00/Fixed.hpp
@@ -1,11 +1,20 @@
 #ifndef FIXED_HPP
 #define FIXED_HPP
+
+#include <iostream>
+
 class Fixed
 {
 private:
-	/* data */
+	int _fixedNumber;
+	const static int _fractionNum;
+
 public:
 	Fixed(void);
-	~Fixed();
+	Fixed(const Fixed &toCopy);
+	Fixed &operator=(const Fixed &toCopy);
+	int getRawBits( void ) const;
+	void setRawBits( int const raw );
+	~Fixed(void);
 };
 #endif
