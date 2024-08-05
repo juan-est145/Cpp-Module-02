cf9f481576a98e0da1da106e3c471055de3aff2b
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-08-05 20:48:37 +0200

    Implemented the plus operator

--- ex02/Fixed.hpp
+++ ex02/Fixed.hpp
@@ -26,6 +26,7 @@ public:
 	bool operator<=(const Fixed &element) const;
 	bool operator==(const Fixed &element) const;
 	bool operator!=(const Fixed &element) const;
+	Fixed operator+(const Fixed &element) const;
 	~Fixed(void);
 };
 std::ostream &operator<<(std::ostream &out, const Fixed &element);
